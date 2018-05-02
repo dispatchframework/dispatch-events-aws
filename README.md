3637d35454e64bfeb5ad4909de71d7d3cf2fe4b2
ZimengYang <zmyang2011@gmail.com>
2018-05-02 10:44:11 -0700

    Automate AWS resources creation (#2)
    * Automate AWS resources creation

--- README.md
+++ README.md
@@ -1,55 +1,63 @@
 ## Dispatch Event Driver for AWS
-This repo contains a template for aws SQS event driver support for Dispatch.
+This repo contains a template for aws SQS event driver for Dispatch.
 
 ### 1. Get AWS credentials
-Follow [`Get AWS Access Key`](https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/setting-up.html) to obtain aws credential, take a note for `<aws_access_key_id>` and `<aws_secret_access_key>`. Create env varbiable for these two fields:
+Follow [`Get AWS Access Key`](https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/setting-up.html) to obtain aws region and credential, take a note for `<aws_region>`, `<aws_access_key_id>` and `<aws_secret_access_key>`. Create env varbiable for these two fields:
 ```bash
 $ export AWS_AKID=<aws_access_key_id>
 $ export AWS_SECRET_KEY=<aws_secret_access_key>
 ```
 
-### 2. Create AWS SQS Queue *[will be managed by Dispatch]*
-Create a standard AWS SQS with name *dispatch*, and also enable `long-polling` by setting `Receive Message Wait Time` attribute to 20.
-
-For more details, refer to [SQS long-polling](https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-long-polling.html)
-
-
-### 3. Create Rules in AWS CloudWatch *[will be managed by Dispatch]*
-In *AWS console* -> *CloudWatch* -> *Event* -> *Rules* -> *Create Rules* for the event that want to be received by Dispatch. Choose *AWS Console Sign-in* -> *Sign-in Events* for example
-
-For Event Source, take a note of the `source` field of the chosen event, this will be used in creation of Dispatch subscription later.
-> **Note:** Most aws events display `source` field under *Event Patter Preview* section, if not, please expand *Show sample events* and look for `source` field.
-
-For Event Targets, choose the SQS queue *dispatch* created in step 2.
-
-
-### 4. Build Event Driver Image
+### 2. Build Event Driver Image
 ```bash
 $ docker build -t dispatch-events-aws .
 ```
 
-### 5. Create Eventdrivertype in Dispatch
+### 3. Create Eventdrivertype in Dispatch
 ```bash
 $ dispatch create eventdrivertype aws-sqs dispatch-events-aws:latest
 Created event driver type: aws-sqs
 ```
 
-### 6. Create Eventdriver in Dispatch
+### 4. Create Eventdriver in Dispatch
+When creating AWS-eventdriver, following arguments should be specified properly:
+* `region`, `access-key-id` and `secret-key` : obtained from step 1
+* Ways to invoke target: *Event Pattern* or *Schedule*:
+  * `event-pattern` : setting up event pattern for AWS CloudWatch Rule, refer to [AWS Event Pattern](https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/CloudWatchEventsandEventPatterns.html) for more details.
+  * `schedule-expression` : specifying event schedule expression, please refer to [AWS Schedule Events](https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html) for details.
+
+Following parameters are optional (have defaults):
+* `rule-name` : specifying CloudWatch Rule name, default "*dispatch*"
+* `queue-name` : specifying SQS queue name, default "*dispatch*"
+* `duration` : specifying SQS polling message duration, default "*20*" (0-20 seconds), which enables [Long-polling](https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-long-polling.html)
+* `namespace` : specifying event namespace, default "*dispatchframework.io/aws-event*"
+* `source-id` : specifying event source id, default a new UUID string
+* `clean-up` : clean up AWS resources after Driver shuts down. default *false*
+
+
+For example, create a event driver by `event-pattern`:
 ```bash
-$ dispatch create eventdriver aws-sqs --set access-key-id=${AWS_AKID} --set secret-key=${AWS_SECRET_KEY}
+$ dispatch create eventdriver aws-sqs --set region="us-west-2" --set access-key-id=${AWS_AKID} --set secret-key=${AWS_SECRET_KEY} --set clean-up="true" --set event-pattern="{\"source\":[\"aws.autoscaling\"]}"
 Created event driver: holy-grackle-805996
 ```
 
-### 7. Create Subscription in Dispatch
-For example, create a `aws.signin` event subscription:
+or by `schedule-expression`:
 ```bash
-$ dispatch create subscription hello-py --event-type="aws.signin" --source-type="aws"
+$ dispatch create eventdriver aws-sqs --set region="us-west-2" --set access-key-id=${AWS_AKID} --set secret-key=${AWS_SECRET_KEY} --set clean-up="true" --set schedule-expression="rate(1 minute)"
+Created event driver: trusting-marlin-576200
+```
+
+
+### 5. Create Subscription in Dispatch
+For example, create a `aws.autoscaling` event subscription:
+```bash
+$ dispatch create subscription hello-py --event-type="aws.autoscaling" --source-type="aws"
 created subscription: innocent-werewolf-420270
 ```
 
 `source-type` should be `aws`.
 
-`event-type` should be the AWS event source type that the subscription will be listening to. This attribute can be found in AWS Console -> CloudWatch -> Events -> Rules -> Create Rules -> Event Source -> Event Pattern -> `source` attribute in event pattern preview.
+`event-type` should be the AWS event source type that the subscription will be listening to. Please refer to [AWS Event Types](https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/EventTypes.html) for more details.
 
-> **NOTE:** In subscription, `source-type` and `event-type` must match the CloudEvent attributes
+> **NOTE:** In subscription, `source-type` and `event-type` must match the CloudEvent attributes, otherwise Dispatch won't receive the event
 
--- driver.go
+++ driver.go
@@ -11,12 +11,15 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchevents"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	uuid "github.com/satori/go.uuid"
 
@@ -25,39 +28,84 @@ import (
 )
 
 var sess *session.Session
-var svc *sqs.SQS
+var sqsService *sqs.SQS
+var cweService *cloudwatchevents.CloudWatchEvents
 var driverClient driverclient.Client
-var queueURL string
+var sqsQueueURL *string
 
+// AWS args
 var awsRegion = flag.String("region", "us-west-2", "Set aws region")
 var awsAKId = flag.String("access-key-id", "", "AWS credential access id [aws_access_key_id] field")
 var awsSecretKey = flag.String("secret-key", "", "AWS credential secret access key [aws_secret_access_key] field")
-var sqsQueueName = flag.String("queue-name", "dispatch", "Set SQS queue name")
+
+// CloudWatch args
+var ruleName = flag.String("rule-name", "dispatch", "Rule name in CloudWatch event")
+var eventPattern = flag.String("event-pattern", "", "Event pattern for AWS CloudWatch Rule, should be a JSON string, for example: {\"source\":[\"aws.autoscaling\"]}")
+var scheduleExpression = flag.String("schedule-expression", "", "Schedule expression, For example, cron(0 20 * * ? *) or rate(5 minutes).")
+
+// SQS args
+var sqsQueueName = flag.String("queue-name", "dispatch", "Set SQS queue name, will be used as CloudWatch Rule target")
 var fetchDuration = flag.Int64("duration", 20, "Fetching duration in seconds")
 
+// Dispatch Event args
 var eventNamespace = flag.String("namespace", "dispatchframework.io/aws-event", "Set event namespace")
 var eventSourceID = flag.String("source-id", uuid.NewV4().String(), "Set custom Source ID for the driver")
+
+// clean up resources after shutting down
+var cleanUp = flag.Bool("clean-up", false, "Clean up AWS resources after shutting down")
+
+// debug
+var dryRun = flag.Bool("dry-run", false, "Debug, pull messages and do not send Dispatch events")
 
 func getSession() *session.Session {
 	return session.Must(session.NewSessionWithOptions(session.Options{
 		Config: aws.Config{
-			Region:      aws.String(*awsRegion),
+			Region:      awsRegion,
 			Credentials: credentials.NewStaticCredentials(*awsAKId, *awsSecretKey, ""),
 		},
 	}))
 }
 
-func getQueueURL(queueName *string) string {
-	urlRes, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{
-		QueueName: aws.String(*queueName),
+func getQueueURL(queueName *string) (url *string) {
+
+	urlRes, err := sqsService.GetQueueUrl(&sqs.GetQueueUrlInput{
+		QueueName: queueName,
 	})
+
 	if err != nil {
-		panic(err)
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case sqs.ErrCodeQueueDoesNotExist:
+				log.Printf("Given SQS queue doesn't exist, creating one ..\n")
+				longPollingDuration := strconv.Itoa(20)
+
+				// Create queue with long-polling enabled
+				createOutputs, err := sqsService.CreateQueue(&sqs.CreateQueueInput{
+					QueueName: queueName,
+					Attributes: map[string]*string{
+						"ReceiveMessageWaitTimeSeconds": &longPollingDuration,
+					},
+				})
+				if err != nil {
+					panic(err)
+				}
+				log.Printf("Queue - %s created.\n", *queueName)
+				url = createOutputs.QueueUrl
+			}
+		} else {
+			panic(err)
+		}
+	} else {
+		url = urlRes.QueueUrl
 	}
-	return *urlRes.QueueUrl
+	return
 }
 
 func handleEvent(m *sqs.Message) {
+	if *dryRun {
+		return
+	}
+
 	byt := []byte(*m.Body)
 	var dat map[string]interface{}
 	if err := json.Unmarshal(byt, &dat); err != nil {
@@ -83,33 +131,167 @@ func handleEvent(m *sqs.Message) {
 	}
 }
 
-func receiveAndDeleteMessage(qN string) error {
+func receiveAndDeleteMessage(qURL *string) (e error) {
 
-	var result error
 	// Receive message
 	maxNumberOfMessages := int64(10)
-	receiveOutput, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
-		QueueUrl:            &queueURL,
+	receiveOutput, err := sqsService.ReceiveMessage(&sqs.ReceiveMessageInput{
+		QueueUrl:            qURL,
 		MaxNumberOfMessages: &maxNumberOfMessages,
 	})
 	if err != nil {
 		log.Printf("Receive message failed: %s\n", err.Error())
-		result = err
+		e = err
 	} else {
 		for _, m := range receiveOutput.Messages {
+			log.Printf("Received Event: %s\n", m.String())
 			handleEvent(m)
 			// Delete message
-			_, err := svc.DeleteMessage(&sqs.DeleteMessageInput{
-				QueueUrl:      &queueURL,
+			_, err := sqsService.DeleteMessage(&sqs.DeleteMessageInput{
+				QueueUrl:      qURL,
 				ReceiptHandle: m.ReceiptHandle,
 			})
 			if err != nil {
 				log.Printf("Delete message failed: %s\n", err.Error())
-				result = err
+				e = err
 			}
 		}
 	}
-	return result
+	return
+}
+
+func getDriverClient() driverclient.Client {
+	if *dryRun {
+		return nil
+	}
+
+	client, err := driverclient.NewHTTPClient()
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Event driver initialized.")
+	return client
+}
+
+func addQueuePermission(rArn, qURL, qArn *string) error {
+	// Add required permission to sqs target
+	// https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/resource-based-policies-cwe.html
+	// https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/iam-example-policies.html
+	type (
+		ConditionEntry struct {
+			ArnEquals map[string]string
+		}
+
+		StatementEntry struct {
+			Sid       string
+			Effect    string
+			Action    string
+			Resource  string
+			Condition ConditionEntry
+			Principal map[string]string
+		}
+
+		PolicyEntry struct {
+			Version   string
+			Id        string
+			Statement []StatementEntry
+		}
+	)
+	policy := PolicyEntry{
+		Version: "2012-10-17",
+		Id:      *qArn + "/SQSDefaultPolicy",
+		Statement: []StatementEntry{
+			StatementEntry{
+				Sid:      "TrustCWEToSendEventsToSQS",
+				Effect:   "Allow",
+				Action:   "sqs:SendMessage",
+				Resource: *qArn,
+				Condition: ConditionEntry{
+					ArnEquals: map[string]string{
+						"aws:SourceArn": *rArn,
+					},
+				},
+				Principal: map[string]string{
+					"Service": "events.amazonaws.com",
+				},
+			},
+		},
+	}
+	policyBytes, _ := json.Marshal(policy)
+	policyString := string(policyBytes)
+	log.Printf("Policy: %s\n", policyString)
+
+	_, err := sqsService.SetQueueAttributes(&sqs.SetQueueAttributesInput{
+		QueueUrl: qURL,
+		Attributes: map[string]*string{
+			"Policy": aws.String(policyString),
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Attributes set")
+	return nil
+}
+
+func putRuleAndTarget(rN, pattern, qURL, qName *string) {
+
+	// put rule
+	var (
+		putRuleOutput *cloudwatchevents.PutRuleOutput
+		err           error
+	)
+	if *scheduleExpression == "" {
+		putRuleOutput, err = cweService.PutRule(&cloudwatchevents.PutRuleInput{
+			Name:         rN,
+			Description:  aws.String("Dispatch AWS event driver rule with EventPattern"),
+			EventPattern: pattern,
+			State:        aws.String(cloudwatchevents.RuleStateEnabled),
+		})
+	} else {
+		putRuleOutput, err = cweService.PutRule(&cloudwatchevents.PutRuleInput{
+			Name:               rN,
+			Description:        aws.String("Dispatch AWS event driver rule with Schedule Experssion"),
+			ScheduleExpression: scheduleExpression,
+			State:              aws.String(cloudwatchevents.RuleStateEnabled),
+		})
+	}
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("PutRuleOutput: %s\n", putRuleOutput.String())
+
+	// get queue arn
+	queueArnAttribute := string("QueueArn")
+	getQueueAttributeOutput, _ := sqsService.GetQueueAttributes(&sqs.GetQueueAttributesInput{
+		QueueUrl: qURL,
+		AttributeNames: []*string{
+			&queueArnAttribute,
+		},
+	})
+	queueArn := getQueueAttributeOutput.Attributes[queueArnAttribute]
+	log.Printf("Queue ARN: %s \n", *queueArn)
+
+	// before put target, add permission to sqs target. Allow CloudWatch to SendMessage to sqs.
+	if err := addQueuePermission(putRuleOutput.RuleArn, qURL, queueArn); err != nil {
+		log.Printf("Failed to add permission to SQS queue: %s. Please make sure user has required privileges. \n", err.Error())
+	}
+
+	// put queue as target in rule
+	_, err = cweService.PutTargets(&cloudwatchevents.PutTargetsInput{
+		Rule: rN,
+		Targets: []*cloudwatchevents.Target{
+			&cloudwatchevents.Target{
+				Id:  qName,
+				Arn: queueArn,
+			},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Put rule and target done. \n")
 }
 
 func prepare() {
@@ -118,19 +300,48 @@ func prepare() {
 
 	// init aws session and service
 	sess = getSession()
-	svc = sqs.New(sess)
-	queueURL = getQueueURL(sqsQueueName)
+	sqsService = sqs.New(sess)
+	cweService = cloudwatchevents.New(sess)
+
+	// get SQS url or create new one
+	sqsQueueURL = getQueueURL(sqsQueueName)
+
+	// put rule
+	putRuleAndTarget(ruleName, eventPattern, sqsQueueURL, sqsQueueName)
 
 	// init Dispatch driver client
-	var err error
-	driverClient, err = driverclient.NewHTTPClient()
-	if err != nil {
-		panic(err)
+	driverClient = getDriverClient()
+}
+
+func cleanUpResources(rName, qURL, qName *string) {
+	log.Println("Cleaning up AWS resources...")
+	_, err := sqsService.DeleteQueue(&sqs.DeleteQueueInput{
+		QueueUrl: qURL,
+	})
+	if err == nil {
+		log.Println("SQS Queue deleted.")
 	}
-	log.Println("Event driver initialized.")
+
+	_, err = cweService.RemoveTargets(&cloudwatchevents.RemoveTargetsInput{
+		Rule: rName,
+		Ids: []*string{
+			qName,
+		},
+	})
+	_, err = cweService.DeleteRule(&cloudwatchevents.DeleteRuleInput{
+		Name: rName,
+	})
+	if err == nil {
+		log.Println("CloudWatch Rule deleted.")
+	}
 }
 
 func main() {
+	defer func() {
+		if *cleanUp {
+			cleanUpResources(ruleName, sqsQueueURL, sqsQueueName)
+		}
+	}()
 
 	prepare()
 
@@ -143,7 +354,7 @@ func main() {
 		select {
 		case <-ticker.C:
 			log.Println("Fetching new messages...")
-			go receiveAndDeleteMessage(*sqsQueueName)
+			go receiveAndDeleteMessage(sqsQueueURL)
 		case <-loopDone:
 			log.Printf("Shutting down event driver...")
 			return
